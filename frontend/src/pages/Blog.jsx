25714e35b93acf93d57bdbcb1456ec72ec592256
shonesjn <shonesajanstjohns@gmail.com>
2025-03-15 17:46:59 +0530

    changeblogpage

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 function Blog() {
   return (
-    <div>
-      this is blog page
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-6 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold">Blog Page</h1>
+        <p className="text-gray-600 mt-2">Welcome to the blog page!</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Blog
+export default Blog;
